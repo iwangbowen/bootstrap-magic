e33800b8e5444299d774b9fe920e85b9675580f8
Bowen Wang <iwangbowen@gmail.com>
2019-01-09 10:18:04 +0800

    Check themes dir exists

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
-const express        = require('express');
-const bodyParser     = require('body-parser');
-const cors           = require('cors');
-const app            = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fs = require('fs');
+const app = express();
 const port = 8088;
+const themesDir = './themes';
+
+// Create themes folder if not exists
+fs.exists(themesDir, (exists) => {
+    if (!exists) {
+        fs.mkdir(themesDir, (err) => {
+            if (err) {
+                console.error(err);
+            }
+        });
+    }
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
